72ecdcb72e2d3b04ec34229cff45ae030c14ddb1
NikitaLib <kykylib107@gmail.com>
2023-01-21 21:40:54 +0200

    I did how you said.

--- src/SecondPractise/Filter.java
+++ src/SecondPractise/Filter.java
@@ -6,12 +6,18 @@ public interface Filter {
     List<Object> getModificatedList(List<Object> list);
 
     static String getCleanString(String string){
+        if(string.toLowerCase().trim().equals("true")){
+            return "false";
+        }
+        if(string.toLowerCase().trim().equals("false")){
+            return "true";
+        }
         return string.trim();
     }
     static int getIntegerNumber(Double someNumber){
         return someNumber.intValue();
     }
     static boolean getReverseBoolean(boolean bool){
-        return !bool;
+            return !bool;
     }
 }
--- src/SecondPractise/Main.java
+++ src/SecondPractise/Main.java
@@ -10,6 +10,7 @@ public class Main {
         List<Object> someList = new ArrayList<>();
 
         someList.add(" Hello  ");
+        someList.add(" True  ");
         someList.add(null);
         someList.add(10);
         someList.add(2.5);
@@ -17,55 +18,24 @@ public class Main {
         someList.add(false);
 
         Filter filter = list -> {
-            // Second version
             List<Object> filteredList = new ArrayList<>();
             for (Object object : list) {
-                Object data = Optional.ofNullable(object).orElseGet(() -> {
-                    filteredList.add(System.currentTimeMillis());
-                    return Optional.empty();
-                });
-                Optional.ofNullable(data)
+                Object data = Optional.ofNullable(object)
                         .map(currentData -> {
-                            Optional.ofNullable(currentData)
-                                    .filter(someData -> data.getClass() == String.class)
-                                    .ifPresent(someData ->filteredList.add(Filter.getCleanString((String) someData)));
+                            if (currentData.getClass() == String.class) {
+                                return Filter.getCleanString((String) currentData);
+                            }
+                            if (currentData.getClass() == Double.class || currentData.getClass() == Float.class) {
+                                return Filter.getIntegerNumber((Double) currentData);
+                            }
+                            if (currentData.getClass() == Boolean.class) {
+                                return Filter.getReverseBoolean((boolean) currentData);
+                            }
                             return currentData;
                         })
-                        .map(currentData -> {
-                            Optional.ofNullable(currentData)
-                                    .filter(someData -> data.getClass() == Double.class)
-                                    .ifPresent(someData -> filteredList.add(Filter.getIntegerNumber((Double) someData)));
-                            return currentData;
-                        })
-                        .map(currentData -> {
-                            Optional.ofNullable(currentData)
-                                    .filter(someData -> data.getClass() == Boolean.class)
-                                    .ifPresent(someData -> filteredList.add(Filter.getReverseBoolean((Boolean) someData)));
-                            return currentData;
-                        })
-                        .filter(someData -> someData.getClass() != String.class && someData.getClass() != Double.class && someData.getClass() != Boolean.class && someData != Optional.empty())
-                        .ifPresent(someData -> filteredList.add(someData));
-// First version
-//                Optional.ofNullable(object)
-//                        .map(data -> {
-//                            Optional.ofNullable(data)
-//                            .ifPresent(someData -> {
-//                                if (someData.getClass() == String.class) {
-//                                    filteredList.add(Filter.getCleanString((String) someData));
-//                                }
-//                                else if (someData.getClass() == Double.class) {
-//                                    filteredList.add(Filter.getIntegerNumber((Double) someData));
-//                                }
-//                                else if (someData.getClass() == Boolean.class) {
-//                                    filteredList.add(Filter.getReverseBoolean((Boolean) someData));
-//                                }
-//                                else {
-//                                    filteredList.add(someData);
-//                                }
-//                            });
-//                            return data;
-//                        })
-//                        .orElseGet(() -> filteredList.add(System.currentTimeMillis()));
+                        .filter(currentData -> currentData != Optional.empty())
+                        .orElse(System.currentTimeMillis());
+                filteredList.add(data);
             }
             return filteredList;
         };
